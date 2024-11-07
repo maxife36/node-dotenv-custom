e588cc33a978b13117da26086afad0f3b8802a68
maxife36 <maxife36@gmail.com>
2024-11-07 16:00:15 -0300

    modify the export to be es6 and commonJs compliant

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import CustomENV from "./configEnv"
 import * as T from "../types";
 
 
-export default function customEnvInit(config?: T.Config){
+function customEnvInit(config?: T.Config){
     const customEnv =  new CustomENV(config)
     return customEnv.env
 }
+
+module.exports = customEnvInit
